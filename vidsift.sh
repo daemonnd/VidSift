c06c8d1f385ccfe82b8bc34f66c12fa323e7466e
daemonnd <daemonnd@protonmail.com>
2026-03-13 21:33:40 +0100

    add vidsift.sh, main and orchestration script

---
+++ vidsift.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# main script
+# executes and orchestrates all the others
+
+# strict mode
+set -Eeuo pipefail
+
+# Cleanup function
+function cleanup {
+    local exit_code="$?"
+    echo "Script interupted or failed. Cleaning up..."
+
+    # remove tmp files
+
+    # exit the script, preserving the exit code
+    exit "$exit_code"
+}
+
+# trap errors
+trap 'echo "Error on line $LINENO: command \"$BASH_COMMAND\" exited with status $?" >&2' ERR
+# trap signals
+trap 'cleanup' INT TERM ERR
+
+function check_args {
+    :
+}
+
+function main {
+    ./url_collector.sh | ./video_validator.sh
+}
+
+# call main with all args, as given
+main "$@"
