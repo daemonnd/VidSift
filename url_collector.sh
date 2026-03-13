874e0ffeb6d68943f3fd2dc4a0e39cf0127a3d63
daemonnd <daemonnd@protonmail.com>
2026-03-13 17:17:47 +0100

    add url_collector with having the channelid and name as vars

---
+++ url_collector.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# strict mode
+set -Eeuo pipefail
+
+# Cleanup function
+function cleanup {
+    local exit_code="$?"
+    echo "Script interupted or failed. Cleaning up..."
+
+    # remove tmp files
+
+    # exit the script, preserving the exit code
+    exit "$exit_code"
+}
+
+# trap errors
+trap 'echo "Error on line $LINENO: command \"$BASH_COMMAND\" exited with status $?" >&2' ERR
+# trap signals
+trap 'cleanup' INT TERM ERR
+
+function check_args {
+    echo
+}
+
+function main {
+    while read -r name channelid; do
+        echo "$name"
+        echo "$channelid"
+    done < <(jq -r "to_entries[]"' | [.key, .value] | @tsv' channelids.json | awk ' {print $1, $2} ')
+}
+
+# call main with all args, as given
+main "$@"
