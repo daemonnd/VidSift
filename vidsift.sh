b8c09bf7e338e550eafec8a8025271f6bdaebf88
daemonnd <daemonnd@protonmail.com>
2026-03-15 14:03:26 +0100

    add script names when they exit with != 0 for better debugging

--- vidsift.sh
+++ vidsift.sh
@@ -8,7 +8,7 @@ set -Eeuo pipefail
 # Cleanup function
 function cleanup {
     local exit_code="$?"
-    echo "Script interupted or failed. Cleaning up..."
+    echo "Script vidsift.sh interupted or failed. Cleaning up..."
 
     # remove tmp files
 
@@ -17,7 +17,7 @@ function cleanup {
 }
 
 # trap errors
-trap 'echo "Error on line $LINENO: command \"$BASH_COMMAND\" exited with status $?" >&2' ERR
+trap 'echo "Error on line $LINENO in vidsift.sh: command \"$BASH_COMMAND\" exited with status $?" >&2' ERR
 # trap signals
 trap 'cleanup' INT TERM ERR
 
