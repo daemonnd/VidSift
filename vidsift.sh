ab82981f0365e1c95b5cfb5cffa69c243c5e75a8
daemonnd <daemonnd@protonmail.com>
2026-03-15 15:01:51 +0100

    move score result of 80 to ai summary from download in vidsift.sh

--- vidsift.sh
+++ vidsift.sh
@@ -37,9 +37,9 @@ function main {
     while read -r url; do
         echo "url: $url"
         score=$(./video_validator.sh "$url")
-        if [[ "$score" -gt 80 || "$score" -eq 80 ]]; then
+        if [[ "$score" -gt 80 ]]; then
             echo "The video with the url $url would have been downloaded, because its score is $score"
-        elif [[ "$score" -lt 80 && "$score" -gt 40 ]]; then
+        elif [[ "$score" -lt 80 && "$score" -gt 40 || "$score" -eq 80 ]]; then
             echo "The video with the url $url would have been summarized, because its score is $score"
         else
             echo "Then video with the url $url would neither have been summarized nor been downloaded, because its score is $score"
