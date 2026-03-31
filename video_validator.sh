a11ee822bfa12d852de5cb2a7e67885abc1ef930
daemonnd <daemonnd@protonmail.com>
2026-03-31 16:01:44 +0200

    remove the video data fetching in video_validator.sh

--- video_validator.sh
+++ video_validator.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # file for validating the video. That includes:
-# - fetching the transcript of the video
 # - running fabric with a custom pattern against the transcript and get the score
 # - return the action that should be performed
 
@@ -24,18 +23,17 @@ trap 'echo "Error on line $LINENO video_validator.sh: command \"$BASH_COMMAND\" 
 trap 'cleanup' INT TERM ERR
 
 function check_args {
+    : "${1:?ERROR: The first arg that should contain the video url has not been set}"
     : "${2:?ERROR: The second arg that should contain the creator name has not been set}"
 }
 
-function fetch_transcript {
-    if ! transcript="$(fabric -y "$1" --yt-dlp-args="--cookies-from-browser firefox")"; then
-        return 1
-    fi
+function init {
+    transcript="$(cat /tmp/vidsift_transcript.txt)"
 }
 
 function get_custom_instuctions {
     # function to get the channel-specific instructions for ai validation
-    if [[ -r "./custom_channel_instructions/$1.md" ]]; then
+    if [[ -r "$VIDSIFT_CONFIG_DIR/custom_channel_instructions/$1.md" ]]; then
         custom_channel_instructions="$(cat ./custom_channel_instructions/$1.md)"
     else
         custom_channel_instructions=""
@@ -55,7 +53,7 @@ function rate_video {
     score=$(echo "$transcript" | fabric -sp vidsift_score_youtube_transcript)
     # Check if score is a number
     if [[ "$score" =~ ^[0-9]+$ ]]; then
-        score="$score"
+        :
     else
         score=-2
     fi
@@ -63,20 +61,12 @@ function rate_video {
     if [[ ! "$score" -ge 0 && ! "$score" -le 100 ]]; then
         score=-1
     fi
-    # write the transcript to a file if it should be summarized
-    if [[ "$score" -lt 80 && "$score" -gt 40 || "$score" -eq 80 ]]; then
-        echo "$transcript" >/tmp/vidsift_transcript.txt
-        yt-dlp --skip-download -O '%(title)s' "$1" >/tmp/vidsift_title.txt
-    fi
     echo "$score"
 
 }
 
 function main {
-    if ! fetch_transcript "$1"; then
-        echo -1
-        return 0
-    fi
+    init "$@"
     check_args "$@"
     get_custom_instuctions "$2"
     create_final_system_prompt
